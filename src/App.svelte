24af0a536d9d5fc396434e1020239aa6d7d69f99
rogelio1502 <rogelio15021515@gmail.com>
2022-04-28 23:47:34 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+	import axios from 'axios'
+	import { Router, Route, Link } from "svelte-navigator";
+	import Login from "./Login.svelte";
+	import PrivateRoute from "./PrivateRoute.svelte";
+	import { user } from "./store";
+  
+	function handleLogout() {
+	  $user = null;
+	}
+
+	function print() {
+		axios.get('https://pokeapi.co/api/v2/pokemon/ditto?limit=10').then(
+			(response)=>{
+				console.log(response.data);
+			}
+		)
+	}
+
+
+
+  </script>
+  
+  <Router>
+	<header>
+	  <h1>History</h1>
+  
+	  <nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+		<Link to="profile">Profile</Link>
+	  </nav>
+	</header>
+  
+	<main>
+		<button on:click={print}>
+
+		</button>
+	  <Route path="login">
+		<Login />
+	  </Route>
+  
+	  <Route path="/">
+		<h3>Home</h3>
+		<p>Home sweet home...</p>
+	  </Route>
+  
+	  <Route path="about">
+		<h3>About</h3>
+		<p>That's what it's all about!</p>
+	  </Route>
+  
+	  <PrivateRoute path="profile" let:location>
+		<h3>Welcome {$user.username}</h3>
+		<button on:click={handleLogout}>Logout</button>
+	  </PrivateRoute>
+	</main>
+  </Router>
